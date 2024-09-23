70705fb303ad26bad7ec5ec0d75c65c0ebb94ee7
jawid-fakhari <jawid_fakhari@live.it>
2024-09-23 17:17:51 +0200

    Mapping shadows per un performance migliore

--- src/script.js
+++ src/script.js
@@ -439,7 +439,27 @@ for (const grave of graveGroup.children) {
   grave.receiveShadow = true;
 }
 
+//Mapping
+directionalLight.shadow.mapSize.height = 256;
+directionalLight.shadow.mapSize.width = 256;
+directionalLight.shadow.camera.top = 8;
+directionalLight.shadow.camera.right = 8;
+directionalLight.shadow.camera.bottom = -8;
+directionalLight.shadow.camera.left = -8;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.left = 500;
 
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.camera.far = 10;
+
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.camera.far = 10;
+
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.camera.far = 10;
 
 /************************************************************
  * Animate
